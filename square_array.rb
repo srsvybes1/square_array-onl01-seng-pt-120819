b1a23187dd9034f76aa726e8a710c38a1612a56d
Cody Toney-Griffith <cody.toney@gmail.com>
2019-12-17 01:32:55 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,6 +1,12 @@
 def square_array(array)
   square_array = [1, 2, 3]
-    array.each do |array| array ** 2
+    array.each do {|array| array << array ** 2 }
 end
 end
 
+#def square_array(array)
+ # Enumerator.new do |y|
+  #  array.each { |e| y << e ** 2 }    
+  #end
+  #.take(array.length)
+#end
